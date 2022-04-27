283f3731677d515c0ffb0b18e8f944b4a77d9240
AlexProtchenko <ialexprotchenko@gmail.com>
2022-04-27 20:01:33 +0300

    startup

--- MailApi/Startup.cs
+++ MailApi/Startup.cs
@@ -1,6 +1,31 @@
-namespace MailApi;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
 
-public class Startup
+
+namespace MailApi
 {
-    
+    public class Startup
+    {
+        private IConfigurationRoot _confstring;
+
+        public Startup(IHostEnvironment hostEnv)
+        {
+            _confstring = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsetting.json")
+                .Build();
+        }
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<AppDBContent>(options => options.UseNpgsql(_confstring.GetConnectionString("DefaultConnection")));
+            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env) 
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
+            
+        }
+        
+    }
 }
