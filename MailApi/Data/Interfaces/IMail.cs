b6edf257f27751822a198c87e09f7fc73c608e0e
AlexProtchenko <ialexprotchenko@gmail.com>
2022-04-27 20:11:43 +0300

    interface

--- MailApi/Data/Interfaces/IMail.cs
+++ MailApi/Data/Interfaces/IMail.cs
@@ -1,6 +1,12 @@
-namespace Shop.Data.Interfaces;
+using MailApi.Data.Models;
 
-public interface Imail
+namespace MailApi.Data.Interfaces;
+
+public interface IMail
 {
-    
+    void Delete(User selectedItem);
+    bool Delete(Department selectedItem);
+
+    void Add(User contact);
+    void Add(Department department);
 }
